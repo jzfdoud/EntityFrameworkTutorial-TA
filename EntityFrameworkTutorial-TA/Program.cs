57a0c8d5c2d5b760d50e199a7ea531a66c7def5f
Jada <jada.fagan@gmail.com>
2021-02-26 15:20:31 -0500

    Add project files.

---
+++ EntityFrameworkTutorial-TA/Program.cs
@@ -0,0 +1,89 @@
+﻿using EntityFrameworkTutorial_TA.Models;
+using System;
+using System.Linq;
+
+namespace EntityFrameworkTutorial_TA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sctrl = new StudentsController();
+
+
+            var students = sctrl.GetAll();
+            foreach (var s in students)
+            {
+                Console.WriteLine($"{s.Firstname} {s.Lastname}");
+            }
+
+            //var sGreg = new Student
+            //{
+            //    Id = 0,
+            //    Firstname = "Greg",
+            //    Lastname = "Doud",
+            //    StateCode = "OH",
+            //    Sat = 800
+            //};
+            //var sGregNew = sctrl.Create(sGreg);
+
+            //var std = sctrl.GetByPk(76);
+            //std.Firstname = "Gregory";
+            //sctrl.Change(std);
+            //Console.WriteLine($"{std.Id} | {std.Firstname} {std.Lastname}");
+
+            //var studentDeleted = sctrl.Remove(std.Id);
+            //Console.WriteLine($"{studentDeleted}");
+
+
+            //var st = sctrl.GetByPk(76);
+            //Console.WriteLine($"{st.Firstname} {st.Lastname}");
+        }
+        static void Run1() { 
+            #region Day #12 C# - Entity Framework
+
+            var _context = new eddbContext();
+
+
+            #region List all students from EdDb
+            //var students = _context.Students.ToList();
+            foreach (var s in _context.Students.ToList())
+            {
+                Console.WriteLine($"{s.Firstname} {s.Lastname}");
+            }
+            #endregion
+
+            #region Only show Majors with minSAT > 1000
+            var majors = from m in _context.Majors
+                         where m.MinSat > 1000
+                         orderby m.Description
+                         select m;
+            foreach (var m in majors)
+            {
+                Console.WriteLine($"{ m.Description} | { m.MinSat}");
+            }
+            #endregion
+
+            #region Join Students and Majors, print name and major
+
+            var allStudents = (from s in _context.Students
+                               join m in _context.Majors
+                               on s.MajorId equals m.Id into grp
+                               from mm in grp.DefaultIfEmpty()
+                               select new
+                               {
+                                   Name = s.Firstname + " " + s.Lastname,
+                                   Major = mm == null ? "Undeclared" : mm.Description
+                               }).ToList();
+            allStudents.ForEach(s => Console.WriteLine($"{s.Name} | {s.Major}"));
+            #endregion
+
+            #region
+
+            #endregion
+
+            #endregion
+        }
+    }
+}
---
+++ EntityFrameworkTutorial-TA/StudentsController.cs
@@ -0,0 +1,101 @@
+﻿using EntityFrameworkTutorial_TA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntityFrameworkTutorial_TA
+{
+    public class StudentsController
+    {
+        private readonly eddbContext _context;
+
+        public Student Create(Student student)
+        {
+            if (student == null)
+            {
+                throw new Exception("Student cannot be null");
+            }
+            if (student.Id != 0)
+            {
+                throw new Exception("Student Id cannot equal zero");
+            }
+            _context.Students.Add(student);
+            var rowsaffected = _context.SaveChanges();
+            if (rowsaffected != 1)
+            {
+                throw new Exception("Create failed");
+            }
+            return student;
+        }
+
+        public void Change(Student student)
+        {
+            if (student == null)
+            {
+                throw new Exception("Student cannot be null");
+            }
+            if (student.Id <= 0)
+            {
+                throw new Exception("Student Id cannot equal zero");
+            }
+            //_context.Entry(student).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            var rowsaffected = _context.SaveChanges();
+            if (rowsaffected != 1)
+            {
+                throw new Exception("Change failed");
+            }
+            return;
+        }
+
+        public Student Remove(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student == null)
+            {
+                return null;
+            }
+            _context.Students.Remove(student);
+            var rowsaffected = _context.SaveChanges();
+            if (rowsaffected != 1)
+            {
+                throw new Exception("Remove failed");
+            }
+            return student;
+        }
+
+        public IEnumerable<Student> GetAll()
+        {
+            return _context.Students.ToList();
+        }
+
+        public Student GetByPk(int id)
+        {
+            return _context.Students.Find(id);
+            //EF call 'find' only works with call primary key?
+        }
+
+        #region Read all students with SAT 1000-1200(inclusive), sort descending
+
+        public IEnumerable<Student> GetBySATRange(int lowSAT, int highSAT)
+        {
+            return _context.Students.Where(s => s.Sat >= lowSAT && s.Sat <= highSAT).OrderByDescending(s => s.Sat).ToList();
+
+            return (from s in _context.Students
+                    where s.Sat >= lowSAT && s.Sat <= highSAT
+                    orderby s.Sat descending
+                    select s).ToList();
+        }
+
+        #endregion
+
+
+
+        public StudentsController()
+        {
+            _context = new eddbContext();
+        }
+
+        //only place you can set the value of readonly is in constructor
+    }
+}
